e24e32860c0e39d4e98ada5659bfe9e731c33cac
MohammedMashal <115952406+MohammedMashal@users.noreply.github.com>
2023-05-05 08:13:12 +0300

    Program to print inverted full pyramid pattern using stars

---
+++ invert full pyramid.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+    int n;
+    scanf("%i",&n);
+    int num_of_spaces=0;
+    int num_of_stars=n*2-1;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=1;j<=num_of_spaces;j++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=num_of_stars;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        num_of_spaces++;
+        num_of_stars--;
+        num_of_stars--;
+    }
+    return 0;
+}
