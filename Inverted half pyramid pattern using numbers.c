668b5898b4def7a492a17524c7365de7b24346ce
MohammedMashal <115952406+MohammedMashal@users.noreply.github.com>
2023-05-05 08:30:51 +0300

    Program to print inverted half pyramid pattern printing using numbers

---
+++ Inverted half pyramid pattern using numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+    int n;
+    scanf("%i",&n);
+    int counter=n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=counter;j++)
+        {
+            printf("%i ",j);
+        }
+        printf("\n");
+        counter--;
+    }
+    return 0;
+}
