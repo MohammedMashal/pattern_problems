67840780dffecf21b953b8818653ac772c3e6985
MohammedMashal <115952406+MohammedMashal@users.noreply.github.com>
2023-05-08 17:44:28 +0300

    Program for Printing a hollow Rhombus

---
+++ hollow Rhombus Pattern.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int n;
+    scanf("%i",&n);
+    int counter=n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=counter;j++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=n;j++)
+        {
+            if(j==1||j==n||i==1||i==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+        counter--;
+    }
+    return 0;
+}
