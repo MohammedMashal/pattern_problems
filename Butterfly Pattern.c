a06bbf624533e63d53207fe2384deb216d17790c
MohammedMashal <115952406+MohammedMashal@users.noreply.github.com>
2023-05-08 08:40:46 +0300

    Program for Butterfly Pattern

---
+++ Butterfly Pattern.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int n;
+    scanf("%i",&n);
+    int num_of_spaces=n*2-1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        for(int j=1;j<=num_of_spaces;j++)
+        {
+            printf("  ");
+        }
+        for(int j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+        num_of_spaces-=2;
+    }
+    num_of_spaces+=2;
+    for(int i=n;i>=1;i--)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        for(int j=1;j<=num_of_spaces;j++)
+        {
+            printf("  ");
+        }
+        for(int j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+        num_of_spaces+=2;
+    }
+    return 0;
+}
